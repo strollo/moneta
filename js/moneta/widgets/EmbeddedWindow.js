13d2cf3e67a9b4a0bc86e421fd41542f07d9b692
Daniele Strollo <daniele.strollo@gmail.com>
2013-11-09 16:09:17 +0100

    - Embedded window for maximization inside a panel

---
+++ js/moneta/widgets/EmbeddedWindow.js
@@ -0,0 +1,31 @@
+/**
+ * A simple Window extension that is designed to maximize inside a container.
+ * Sample of instantiation: 
+ * Ext.create('moneta.widgets.EmbeddedWindow', '<container ID>', {<custom configuration>});
+ */
+Ext.define('moneta.widgets.EmbeddedWindow', {
+	extend: 'Ext.window.Window',
+	xtype: 'embedded-window',	
+
+	// The default initial configuration
+	config: {
+		maximizable: true,
+	},
+	
+	constructor: function(containerID, cfg) {
+		var me = this;
+		me.renderTo = containerID;
+		me.callParent([Ext.apply(me.config, cfg)]);
+		
+		me.on('maximize', function(self, eOpts) {
+				try {
+					self.setY(self.container.getY());
+				} catch (e) {}
+				// The top Y is not considered when restoring the original position
+				// on unminimize.
+				self.restorePos[1] += self.container.getY();
+				return false;
+			}
+		);
+	}
+});
